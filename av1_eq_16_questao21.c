1fdb887d98196e94e5e64eb4c4e7add20b35ab97
BorgesSanm <149957485+BorgesSanm@users.noreply.github.com>
2023-11-05 13:15:41 -0300

    Update av1_eq_16_questao21.c

--- av1_eq_16_questao21.c
+++ av1_eq_16_questao21.c
@@ -1,1 +1,55 @@
+ #include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    int numeros[100]; // Defina o tamanho máximo da sequência
+    int tamanho = 0;
+    int numero;
+
+    // Leitura da sequência de números
+    printf("Digite uma sequência de números (0 para sair):\n");
+    while (1) {
+        scanf("%d", &numero);
+        if (numero == 0) {
+            break; // Sai do loop se o número for 0
+        }
+        numeros[tamanho] = numero;
+        tamanho++;
+    }
+
+    // Ordenando os números em ordem crescente
+    for (int i = 0; i < tamanho - 1; i++) {
+        for (int j = 0; j < tamanho - i - 1; j++) {
+            if (numeros[j] > numeros[j + 1]) {
+                int temp = numeros[j];
+                numeros[j] = numeros[j + 1];
+                numeros[j + 1] = temp;
+            }
+        }
+    }
+
+    // Imprimindo em ordem crescente
+    printf("Números em ordem crescente: ");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d ", numeros[i]);
+    }
+
+    // Ordenando os números em ordem decrescente
+    for (int i = 0; i < tamanho - 1; i++) {
+        for (int j = 0; j < tamanho - i - 1; j++) {
+            if (numeros[j] < numeros[j + 1]) {
+                int temp = numeros[j];
+                numeros[j] = numeros[j + 1];
+                numeros[j + 1] = temp;
+            }
+        }
+    }
+
+    // Imprimindo em ordem decrescente
+    printf("\nNúmeros em ordem decrescente: ");
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d ", numeros[i]);
+    }
+
+    return 0;
+}
